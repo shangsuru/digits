051d049118cb017d300095aaf43c620951e735a4
shangsuru <helm.henry99@gmail.com>
2020-08-04 19:14:15 +0200

    Show model summary

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ async function main() {
     const data = new DataLoader();
     await data.load();
     await showExamples(data);
+    const model = getModel();
+    tfvis.show.modelSummary({ name: "Model", tab: "Architecture" }, model);
 }
 
 async function showExamples(data) {
@@ -16,9 +18,28 @@ async function showExamples(data) {
         const image = examples.slice([i, 0], [1, examples.shape[1]]);
         const canvas = document.createElement("canvas");
         canvas.className = "examples";
-        tf.browser.toPixels(image.reshape([28, 28, 1]), canvas);
+        await tf.browser.toPixels(image.reshape([28, 28, 1]), canvas);
         surface.drawArea.appendChild(canvas);
     }
+}
+
+function getModel() {
+    const model = tf.sequential();
+    model.add(tf.layers.conv2d({ inputShape: [28, 28, 1], activation: "relu", kernelSize: 3, filters: 32 }));
+    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], stride: [2, 2] }));
+    model.add(tf.layers.conv2d({ inputShape: [28, 28, 1], activation: "relu", kernelSize: 3, filters: 64 }));
+    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], stride: [2, 2] }));
+    model.add(tf.layers.flatten());
+    model.add(tf.layers.dense({ units: 64, activation: "relu" }));
+    model.add(tf.layers.dense({ units: 10, activation: "softmax" }));
+
+    model.compile({
+        optimizer: "rmsprop",
+        loss: "categoricalCrossentropy",
+        metrics: ["accuracy"],
+    });
+
+    return model;
 }
 
 main();
