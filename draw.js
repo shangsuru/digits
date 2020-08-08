2910990c517f2ecb01b6c6f65d8b6424dc72bd7f
shangsuru <helm.henry99@gmail.com>
2020-08-08 21:23:59 +0200

    Do prediction on user input

--- draw.js
+++ draw.js
@@ -1,7 +1,7 @@
-export function initCanvas(canvas) {
+export function initCanvas(canvas, clearButton, predictButton, model) {
     const context = canvas.getContext("2d");
-    canvas.width = 210;
-    canvas.height = 210;
+    canvas.width = 140;
+    canvas.height = 140;
     let mousePressed = false;
     let xPosition;
     let yPosition;
@@ -26,8 +26,7 @@ export function initCanvas(canvas) {
 
             context.beginPath();
             context.strokeStyle = "black";
-            context.lineWidth = 5;
-            context.lineJoin = "round";
+            context.lineWidth = 4;
             context.moveTo(xPositionOld, yPositionOld);
             context.lineTo(xPosition, yPosition);
             context.stroke();
@@ -35,8 +34,70 @@ export function initCanvas(canvas) {
         }
     }
 
+    function clearCanvas() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        prediction.textContent = "";
+    }
+
+    function cropImage(image, width) {
+        image = image.slice([0, 0, 3]);
+        let mask_x = tf.greater(image.sum(0), 0).reshape([-1]);
+        let mask_y = tf.greater(image.sum(1), 0).reshape([-1]);
+        let st = tf.stack([mask_x, mask_y]);
+        let v1 = tf.topk(st);
+        let v2 = tf.topk(st.reverse());
+
+        let [x1, y1] = v1.indices.dataSync();
+        let [y2, x2] = v2.indices.dataSync();
+        y2 = width - y2 - 1;
+        x2 = width - x2 - 1;
+        let crop_width = x2 - x1;
+        let crop_height = y2 - y1;
+
+        if (crop_width > crop_height) {
+            y1 -= (crop_width - crop_height) / 2;
+            crop_height = crop_width;
+        }
+        if (crop_height > crop_width) {
+            x1 -= (crop_height - crop_width) / 2;
+            crop_width = crop_height;
+        }
+
+        image = image.slice([y1, x1], [crop_height, crop_width]);
+        image = image.pad([
+            [6, 6],
+            [6, 6],
+            [0, 0],
+        ]);
+        let resized = tf.image.resizeNearestNeighbor(image, [28, 28]);
+
+        for (let i = 0; i < 28 * 28; i++) {
+            resized[i] = 255 - resized[i];
+        }
+        return resized;
+    }
+
+    function predict() {
+        let image = tf.browser.fromPixels(canvas, 4);
+        let resized = cropImage(image, canvas.width);
+        let x_data = tf.cast(resized.reshape([1, 28, 28, 1]), "float32");
+
+        let y_pred = model.predict(x_data);
+        let prediction = Array.from(y_pred.argMax(1).dataSync());
+
+        document.getElementById("prediction").innerText = prediction;
+
+        const barchartData = Array.from(y_pred.dataSync()).map((d, i) => {
+            return { index: i, value: d };
+        });
+        tfvis.render.barchart(document.getElementById("prediction-graph"), barchartData, { width: 400, height: 140 });
+    }
+
     canvas.addEventListener("mousedown", onMouseDown);
     canvas.addEventListener("mouseup", onMouseUpOrLeave);
     canvas.addEventListener("mouseleave", onMouseUpOrLeave);
     canvas.addEventListener("mousemove", onMouseMove);
+
+    clearButton.onclick = clearCanvas;
+    predictButton.onclick = predict;
 }
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { DataLoader } from "./DataLoader.js";
 import { initCanvas } from "./draw.js";
 
 async function main() {
-    /*const TRAIN_SIZE = 50;
-    const VALIDATION_SIZE = 10;
-    const TEST_SIZE = 10;
+    const TRAIN_SIZE = 5000;
+    const VALIDATION_SIZE = 1000;
+    const TEST_SIZE = 1000;
     const BATCH_SIZE = 512;
     const EPOCHS = 10;
 
@@ -14,33 +14,55 @@ async function main() {
     const [trainImages, trainLabels] = data.getTrainingData();
     const [validationImages, validationLabels] = data.getValidationData();
 
-    await showExamples(testImages, 4);
+    await showExamples(testImages, 40);
     const model = createModel();
     tfvis.show.modelSummary({ name: "Architecture", tab: "Model" }, model);
     await trainModel(model, trainImages, trainLabels, validationImages, validationLabels, BATCH_SIZE, EPOCHS);
 
-    await evaluateModel(model, testImages, testLabels);*/
+    await evaluateModel(model, testImages, testLabels);
 
-    tfvis.visor().surface({ name: "Draw a number", tab: "Testing" });
-    const drawDiv = document.querySelectorAll(".tf-surface:last-child")[0];
-
-    // Create canvas for drawing the number
-    let canvas = document.createElement("canvas");
-    canvas.id = "draw-canvas";
-    initCanvas(canvas);
-    drawDiv.appendChild(canvas);
+    setTimeout(() => createTestingTab(model), 2000);
 }
 
 async function showExamples(examples, numExamples) {
     const surface = tfvis.visor().surface({ name: "Examples", tab: "Dataset", styles: { height: "1000px" } });
     // Loop through every example and show its image
-    for (let i = 0; i < numExamples; i++) {
+    for (let i = 0; i <= numExamples; i++) {
         const image = examples.slice([i, 0], [1, examples.shape[1]]);
         const canvas = document.createElement("canvas");
         canvas.className = "examples";
         await tf.browser.toPixels(image.reshape([28, 28, 1]), canvas);
         surface.drawArea.appendChild(canvas);
     }
+}
+
+function createTestingTab(model) {
+    tfvis.visor().surface({ name: "Draw a number", tab: "Testing" });
+    const drawDiv = document.querySelectorAll(".tf-surface:last-child")[0];
+
+    // Create canvas for drawing the number
+    let buttons = document.createElement("div");
+    let canvas = document.createElement("canvas");
+    canvas.id = "draw-canvas";
+    let clearButton = document.createElement("button");
+    clearButton.textContent = "Clear";
+    buttons.appendChild(clearButton);
+    let predictButton = document.createElement("button");
+    predictButton.textContent = "Predict";
+    buttons.appendChild(predictButton);
+
+    let predictionField = document.createElement("div");
+    predictionField.id = "prediction";
+
+    let predictionGraph = document.createElement("div");
+    predictionGraph.id = "prediction-graph";
+
+    drawDiv.appendChild(predictionField);
+    drawDiv.appendChild(canvas);
+    drawDiv.appendChild(buttons);
+    drawDiv.appendChild(predictionGraph);
+
+    initCanvas(canvas, clearButton, predictButton, model);
 }
 
 function createModel() {
@@ -58,7 +80,7 @@ function createModel() {
     model.add(tf.layers.conv2d({ activation: "relu", kernelSize: 3, filters: 64, kernelInitializer: "varianceScaling" }));
     model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], stride: [2, 2] }));
     model.add(tf.layers.flatten());
-    model.add(tf.layers.dense({ units: 64, activation: "relu", kernelInitializer: "varianceScaling" }));
+    model.add(tf.layers.dense({ units: 64, activation: "sigmoid", kernelInitializer: "varianceScaling" }));
     model.add(tf.layers.dense({ units: 10, activation: "softmax", kernelInitializer: "varianceScaling" }));
 
     model.compile({
